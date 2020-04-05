fd3b32486067d1f05e46cbb1faf612b56c673941
Micael Lopes <micaelopes32@gmail.com>
2020-04-05 15:56:53 -0300

    Testando classe de domínio Categoria

--- src/main/java/com/micaellopes/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/micaellopes/cursomc/resources/CategoriaResources.java
@@ -1,15 +1,27 @@
 package com.micaellopes.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.micaellopes.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST is working";
+	public List<Categoria> listar() {
+		var cat1 = new Categoria(1, "Informática");
+		var cat2 = new Categoria(2, "Escritório");
+		
+		var lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
